bb5400770ab6290c2d763cb133dd4d66ae0e541a
rst0070 <kwb0711@gmail.com>
2021-12-16 11:15:35 +0000

    rest api first commit

---
+++ src/test/java/com/rst0070/openchat/OpenchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rst0070.openchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
